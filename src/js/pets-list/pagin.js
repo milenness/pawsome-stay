f10f482a818c39922d19689a273f113a08da61e5
sergei_romanov <sergemrcat@gmail.com>
2026-03-09 16:14:57 +0200

    update pagination logic

--- src/js/pets-list/pagin.js
+++ src/js/pets-list/pagin.js
@@ -118,31 +118,23 @@ function createDots(targetPage) {
 }
 
 function getVisiblePages(total, current) {
-  if (total <= 5) {
+  if (total <= 4) {
     return Array.from({ length: total }, (_, i) => i + 1);
   }
 
-  if (current <= 4) {
-    return [1, 2, 3, 4, { type: 'dots', page: 5 }, total];
+  if (current <= 3) {
+    return [1, 2, 3, { type: 'dots', page: 4 }, total];
   }
 
-  if (current >= total - 3) {
-    return [
-      1,
-      { type: 'dots', page: total - 4 },
-      total - 3,
-      total - 2,
-      total - 1,
-      total,
-    ];
+  if (current >= total - 1) {
+    return [total - 3, total - 2, total - 1, total];
   }
 
   return [
-    1,
+    current - 2,
     current - 1,
     current,
-    current + 1,
-    { type: 'dots', page: current + 2 },
+    { type: 'dots', page: current + 1 },
     total,
   ];
 }
